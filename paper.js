523eb106fb6547fc6223cb0c3de3d6b75e5ed1a5
Monish <s.monish2006@gmail.com>
2020-12-28 20:34:22 -0500

    CrumpledBall1

---
+++ paper.js
@@ -0,0 +1,17 @@
+class Paper {
+    constructor(x, y){
+    var options={
+        isStatic: false,
+        restitution: 0.3,
+        friction: 0.5,
+        density: 1.2
+    }
+    this.body = Bodies.circle(x, y, 15, options);
+    this.radius = 15;
+    World.add (world, this.body);
+}
+    display() {
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+    }
+}
